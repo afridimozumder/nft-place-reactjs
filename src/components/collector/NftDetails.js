0db7b84e4f424a14d97eac042cca331648ed1752
Afridi Mozumder <49424469+afridimozumder@users.noreply.github.com>
2021-08-18 02:38:51 +0600

    details page + metarial_ui

--- src/components/collector/NftDetails.js
+++ src/components/collector/NftDetails.js
@@ -1,16 +1,44 @@
 import { useParams } from "react-router";
 import { useFetch } from './useFetch';
 import {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import {BasicTextFields } from './metarial';
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 import './css/details.css';
 import Navbar from './Navber';
 
+
+const useStyles = makeStyles((theme) => ({
+    text: {
+        width: '100%',
+        maxWidth: 500,
+      },
+
+    field: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '20ch',
+      },
+    },
+    button: {
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+  }));
+
 const NftDetails = ({itemList}) =>{
     const {id:eid} = useParams();
+    const classes = useStyles();
     
     const url = `http://localhost:8000/api/details/${eid}`;
-    const [userList, setUserList] = useState([]);
-    useFetch(url, setUserList);
+    const [nft, setNftDetails] = useState([]);
+    useFetch(url, setNftDetails);
     
 /*     const deleteUser = (id)=>{
         const List = userList.filter((user)=>user.id !== id);
@@ -22,23 +50,48 @@ const NftDetails = ({itemList}) =>{
         <div class='body'>
             <div class="grid-container">
                 <div class="Navbar"><Navbar/></div>
-                <div class="left">Menu</div>
+                <div class="left"></div>
                 
                 <div class="main">
                     <div class='photo'>
-                        <img src={`/image/${userList.image}`}/>
+                        <img src={`/image/${nft.image}`}/>
                     </div>
 
                     <div class='details'>
-                        <h3>
-                            {userList.name}
 
-                        </h3>
+                        <Typography variant="h4" gutterBottom>
+                            {nft.name}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            {nft.desc}
+                        </Typography>
+
+                        Creator : {nft.creator}<br/>
+                        Edition : {nft.edition}<br/><br/>
+
+                        <Button color="secondary" href={`/proof/${eid}`}>See Proof of Authenticity</Button>
+{/*                         <Button variant="secondary" gutterBottom>
+                            <Link to={`/proof/${eid}`}>See Proof of Authenticity</Link><br/>
+                        </Button> */}
+                        
+                        <h5>Ctagory : Art, Abstract</h5>
+
+                        <Typography variant="h6" gutterBottom>
+                            Initial Price : {nft.value} ETH<br/>
+                        </Typography>
+                        
+                        <BasicTextFields/>
+                        <div className={classes.button}>
+                            <Button variant="outlined" color="secondary" href={`/purchase/${eid}`}>
+                                Submit
+                            </Button>
+                        </div>
+                        
                     </div>
 
                 </div>  
                 
-                <div class="rignt">Right</div>            
+                <div class="rignt"></div>            
                 <div class="footer">Footer</div>
             </div>
         </div>
---
+++ src/components/collector/metarial.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { Edit } from '@material-ui/icons';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles((theme) => ({
+  field: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '20ch',
+    },
+  },
+/*   button: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  }, */
+}));
+
+export function BasicTextFields() {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.field} noValidate autoComplete="off">
+      <TextField id="outlined-basic" label="Make an Offer" variant="outlined" />
+    </form>
+  );
+}
+
+/* export function OutlinedButtons() {
+    const classes = useStyles();
+  
+    return (
+      <div className={classes.root}>
+        <Button variant="outlined" color="secondary" href={`/proof/`}>
+          Submit
+        </Button>
+      </div>
+    );
+  } */
